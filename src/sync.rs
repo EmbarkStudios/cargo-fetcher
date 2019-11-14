21c5eb4ea974fb69573395e0662e4cd7e285650f
Jake Shadle <jake.shadle@embark-studios.com>
2019-11-14 10:50:27 +0100

    Refactor sync

--- src/sync.rs
+++ src/sync.rs
@@ -135,115 +135,101 @@ pub fn locked_crates(ctx: &crate::Ctx) -> Result<usize, Error> {
 
     info!("synchronizing {} missing crates...", to_sync.len());
 
-    to_sync.par_iter().for_each(|krate| {
-        match ctx.backend.fetch(krate) {
+    let sync_package = |krate: &Krate, data: bytes::Bytes, chksum: &str| -> Result<(), Error> {
+        util::validate_checksum(&data, chksum)?;
+
+        let packed_krate_path = cache_dir.join(format!("{}", krate.local_id()));
+
+        {
+            let mut f = std::fs::File::create(&packed_krate_path)
+                .with_context(|| packed_krate_path.display().to_string())?;
+            let _ = f.set_len(data.len() as u64);
+            f.write_all(&data)?;
+        }
+
+        // Decompress and splat the tar onto the filesystem
+        let buf_reader = data.into_buf().reader();
+        let gz_decoder = flate2::read::GzDecoder::new(buf_reader);
+
+        let mut src_path = src_dir.join(format!("{}", krate.local_id()));
+        // Remove the .crate extension
+        src_path.set_extension("");
+        let ok = src_path.join(".cargo-ok");
+
+        if !ok.exists() {
+            if src_path.exists() {
+                log::debug!("cleaning src/ dir for {}", krate);
+                remove_dir_all::remove_dir_all(&src_path)
+                    .with_context(|| format!("unable to remove {}", src_path.display()))?;
+            }
+
+            log::debug!("unpacking {} to src/", krate);
+
+            // Crate tarballs already include the top level directory internally,
+            // so unpack in the top-level source directory
+            util::unpack_tar(gz_decoder, &src_dir).map_err(|(_, e)| e)?;
+
+            // Create the .cargo-ok file so that cargo doesn't suspect a thing
+            std::fs::File::create(&ok).with_context(|| ok.display().to_string())?;
+        }
+
+        Ok(())
+    };
+
+    let sync_git = |krate: &Krate, data: bytes::Bytes, rev: &str| -> Result<(), Error> {
+        let buf_reader = data.into_buf().reader();
+        let zstd_decoder = zstd::Decoder::new(buf_reader)?;
+
+        let db_path = git_db_dir.join(format!("{}", krate.local_id()));
+
+        // The path may already exist, so in that case just do a fetch
+        if db_path.exists() {
+            debug!("fetching bare repo for {}", krate);
+            crate::fetch::update_bare(krate, &db_path)
+                .with_context(|| format!("unable to fetch into {}", db_path.display()))?;
+        } else {
+            util::unpack_tar(zstd_decoder, &db_path)
+                .map_err(|(_, e)| e)
+                .with_context(|| format!("unable to unpack tar into {}", db_path.display()))?;
+        }
+
+        let co_path = git_co_dir.join(format!("{}/{}", krate.local_id(), rev));
+
+        // If we get here, it means there wasn't a .cargo-ok in the dir, even if the
+        // rest of it is checked out and ready, so blow it away just in case as we are
+        // doing a clone/checkout from a local bare repository rather than a remote one
+        if co_path.exists() {
+            debug!("removing checkout dir {} for {}", co_path.display(), krate);
+            remove_dir_all::remove_dir_all(&co_path)
+                .with_context(|| format!("unable to remove {}", co_path.display()))?;
+        }
+
+        // Do a checkout of the bare clone
+        debug!("checking out {} to {}", krate, co_path.display());
+        util::checkout(&db_path, &co_path, rev)?;
+        let ok = co_path.join(".cargo-ok");
+        std::fs::File::create(&ok).with_context(|| ok.display().to_string())?;
+
+        Ok(())
+    };
+
+    to_sync
+        .par_iter()
+        .for_each(|krate| match ctx.backend.fetch(krate) {
             Err(e) => error!("failed to download {}: {}", krate, e),
-            Ok(krate_data) => {
-                match &krate.source {
-                    Source::CratesIo(ref chksum) => {
-                        if let Err(e) = util::validate_checksum(&krate_data, &chksum) {
-                            error!("failed to validate checksum for {}: {}", krate, e);
-                            return;
-                        }
-
-                        let packed_krate_path = cache_dir.join(format!("{}", krate.local_id()));
-
-                        match std::fs::File::create(&packed_krate_path) {
-                            Ok(mut f) => {
-                                let _ = f.set_len(krate_data.len() as u64);
-
-                                if let Err(e) = f.write_all(&krate_data) {
-                                    error!("failed to write {} to disk: {}", krate, e);
-                                }
-                            }
-                            Err(e) => {
-                                error!("failed to create {}: {}", krate, e);
-                            }
-                        }
-
-                        // Decompress and splat the tar onto the filesystem
-                        let buf_reader = krate_data.into_buf().reader();
-                        let gz_decoder = flate2::read::GzDecoder::new(buf_reader);
-
-                        let mut src_path = src_dir.join(format!("{}", krate.local_id()));
-                        // Remove the .crate extension
-                        src_path.set_extension("");
-                        let ok = src_path.join(".cargo-ok");
-
-                        if !ok.exists() {
-                            if src_path.exists() {
-                                log::debug!("cleaning src/ dir for {}", krate);
-                                if let Err(e) = remove_dir_all::remove_dir_all(&src_path) {
-                                    error!("failed to clean dependency {}: {}", krate, e);
-                                    return;
-                                }
-                            }
-
-                            log::debug!("unpacking {} to src/", krate);
-
-                            // Crate tarballs already include the top level directory internally,
-                            // so unpack in the top-level source directory
-                            if let Err((_, e)) = util::unpack_tar(gz_decoder, &src_dir) {
-                                error!("failed to unpack dependency {}: {}", krate, e);
-                                return;
-                            }
-
-                            // Create the .cargo-ok file so that cargo doesn't suspect a thing
-                            if let Err(e) = std::fs::File::create(ok) {
-                                error!("failed to write .cargo-ok: {}", e);
-                            }
-                        }
-                    }
-                    Source::Git { rev, .. } => {
-                        let buf_reader = krate_data.into_buf().reader();
-                        let zstd_decoder = match zstd::Decoder::new(buf_reader) {
-                            Ok(zd) => zd,
-                            Err(e) => {
-                                error!("failed to create decompressor for {}: {}", krate, e);
-                                return;
-                            }
-                        };
-
-                        let db_path = git_db_dir.join(format!("{}", krate.local_id()));
-
-                        // The path may already exist, so in that case just do a fetch
-                        if db_path.exists() {
-                            debug!("fetching bare repo for {}", krate);
-                            if let Err(e) = crate::fetch::update_bare(krate, &db_path) {
-                                error!("failed to fetch crate {}: {}", krate, e);
-                                return;
-                            }
-                        } else if let Err((_, e)) = util::unpack_tar(zstd_decoder, &db_path) {
-                            error!("failed to unpack dependency {}: {}", krate, e);
-                            return;
-                        }
-
-                        let co_path = git_co_dir.join(format!("{}/{}", krate.local_id(), rev));
-
-                        // If we get here, it means there wasn't a .cargo-ok in the dir, even if the
-                        // rest of it is checked out and ready, so blow it away just in case as we are
-                        // doing a clone/checkout from a local bare repository rather than a remote one
-                        if co_path.exists() {
-                            debug!("removing checkout dir {} for {}", co_path.display(), krate);
-                            if let Err(e) = remove_dir_all::remove_dir_all(&co_path) {
-                                error!("failed to remove {}: {}", co_path.display(), e);
-                                return;
-                            }
-                        }
-
-                        // Do a checkout of the bare clone
-                        debug!("checking out {} to {}", krate, co_path.display());
-                        if util::checkout(&db_path, &co_path, rev).is_ok() {
-                            // Tell cargo it totally checked this out itself
-                            if let Err(e) = std::fs::File::create(co_path.join(".cargo-ok")) {
-                                error!("failed to write .cargo-ok: {}", e);
-                            }
-                        }
+            Ok(krate_data) => match &krate.source {
+                Source::CratesIo(ref chksum) => {
+                    if let Err(e) = sync_package(krate, krate_data, chksum) {
+                        error!("unable to synchronize {}: {}", krate, e);
                     }
                 }
-            }
-        }
-    });
+                Source::Git { rev, .. } => {
+                    if let Err(e) = sync_git(krate, krate_data, rev) {
+                        error!("unable to synchronize {}: {}", krate, e);
+                    }
+                }
+            },
+        });
 
     Ok(to_sync.len())
 }
