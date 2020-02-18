146214b4461b2b9e0cb61968d9326a521d952adb
Jake Shadle <jake.shadle@embark-studios.com>
2020-02-18 17:27:29 +0100

    Separate clone into init and fetch

--- src/fetch.rs
+++ src/fetch.rs
@@ -34,20 +34,46 @@ pub async fn via_git(krate: &Krate) -> Result<Bytes, Error> {
 
             debug!("cloning {}", krate);
 
-            let output = Command::new("git")
-                .arg("clone")
+            let init = Command::new("git")
+                .arg("init")
                 .arg("--template")
                 .arg("")
-                //.arg("--no-tags")
                 .arg("--bare")
-                .arg(url.as_str())
                 .arg(temp_dir.path())
                 .output()
                 .await?;
 
-            if !output.status.success() {
-                let err_out = String::from_utf8(output.stderr)?;
-                bail!("failed to clone {}: {}", krate, err_out);
+            if !init.status.success() {
+                let err_out = String::from_utf8(init.stderr)?;
+                bail!("failed to init git repo {}: {}", krate, err_out);
+            }
+
+            let mut fetch = Command::new("git");
+            fetch
+                .arg("fetch")
+                .arg("--tags") // fetch all tags
+                .arg("--force") // handle force pushes
+                .arg("--update-head-ok") // see discussion in #2078
+                .arg(url.as_str())
+                .arg("refs/heads/*:refs/heads/*")
+                // If cargo is run by git (for example, the `exec` command in `git
+                // rebase`), the GIT_DIR is set by git and will point to the wrong
+                // location (this takes precedence over the cwd). Make sure this is
+                // unset so git will look at cwd for the repo.
+                .env_remove("GIT_DIR")
+                // The reset of these may not be necessary, but I'm including them
+                // just to be extra paranoid and avoid any issues.
+                .env_remove("GIT_WORK_TREE")
+                .env_remove("GIT_INDEX_FILE")
+                .env_remove("GIT_OBJECT_DIRECTORY")
+                .env_remove("GIT_ALTERNATE_OBJECT_DIRECTORIES")
+                .current_dir(temp_dir.path());
+
+            let fetch = fetch.output().await?;
+
+            if !fetch.status.success() {
+                let err_out = String::from_utf8(fetch.stderr)?;
+                bail!("failed to fetch git repo {}: {}", krate, err_out);
             }
 
             // Ensure that the revision required in the lockfile is actually present
