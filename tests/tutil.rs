8d7d0b4dd1c1f6368caa9850fd2d19f997d421e8
Jake Shadle <jake.shadle@embark-studios.com>
2020-12-09 06:10:28 +0100

    Oops

---
+++ tests/tutil.rs
@@ -0,0 +1,13 @@
+#![cfg(feature = "fs")]
+
+use cargo_fetcher as cf;
+use std::{path::PathBuf, sync::Arc};
+
+pub async fn fs_ctx(root: PathBuf) -> cf::Ctx {
+    let backend = Arc::new(
+        cf::backends::fs::FSBackend::new(cf::FilesystemLocation { path: &root })
+            .await
+            .expect("failed to create fs backend"),
+    );
+    cf::Ctx::new(None, backend, Vec::new()).expect("failed to create context")
+}
