c11cb4a35ac81fda8e07b640ae05bab2589e31fb
m0ssc0de <hi.paul.q@gmail.com>
2021-07-19 20:47:10 +0800

    replace azure lib with a sans-io style one

--- src/backends/blob.rs
+++ src/backends/blob.rs
@@ -1,14 +1,17 @@
+use crate::backends::gcs::convert_response;
 use crate::Krate;
 use anyhow::{Context, Error};
-use azure_sdk_core::prelude::*;
-use azure_sdk_storage_blob::prelude::*;
-use azure_sdk_storage_core::{client, key_client::KeyClient};
+use bloblock::blob;
 use bytes::Bytes;
+use chrono::Utc;
+use reqwest::Client;
+use std::convert::TryFrom;
 
 #[derive(Debug)]
 pub struct BlobBackend {
     prefix: String,
-    client: KeyClient,
+    instance: blob::Blob,
+    client: Client,
     container: String,
 }
 
@@ -18,10 +21,12 @@ impl BlobBackend {
         account: String,
         master_key: String,
     ) -> Result<Self, Error> {
-        let client = client::with_access_key(&account, &master_key);
+        let instance = blob::Blob::new(&account, &master_key, loc.container, false);
+        let client = reqwest::Client::new();
         Ok(Self {
             prefix: loc.prefix.to_owned(),
             container: loc.container.to_owned(),
+            instance,
             client,
         })
     }
@@ -35,70 +40,87 @@ impl BlobBackend {
 #[async_trait::async_trait]
 impl crate::Backend for BlobBackend {
     async fn fetch(&self, krate: &Krate) -> Result<Bytes, Error> {
-        let response = self
-            .client
-            .get_blob()
-            .with_container_name(&self.container)
-            .with_blob_name(&self.make_key(krate))
-            .finalize()
-            .await
-            .context("failed to fetch object")?;
-        Ok(response.data.into())
+        let dl_req = self.instance.download(
+            &self.make_key(krate),
+            &Utc::now().format("%a, %d %b %Y %T GMT").to_string(),
+        )?;
+        let (parts, _) = dl_req.into_parts();
+
+        let uri = parts.uri.to_string();
+        let builder = self.client.get(&uri);
+
+        let request = builder.headers(parts.headers).build()?;
+
+        let response = self.client.execute(request).await?.error_for_status()?;
+        let res = convert_response(response).await?;
+        let content = res.into_body();
+
+        Ok(content)
     }
 
     async fn upload(&self, source: Bytes, krate: &Krate) -> Result<usize, Error> {
-        let len = source.len();
-        let digest = md5::compute(&source[..]);
-        self.client
-            .put_block_blob()
-            .with_container_name(&self.container)
-            .with_blob_name(&self.make_key(krate))
-            .with_content_type("application/x-tar")
-            .with_body(&source[..])
-            .with_content_md5(&digest[..])
-            .finalize()
-            .await
-            .context("failed to upload object")?;
-        Ok(len)
+        let content_len = source.len() as u64;
+        let insert_req = self.instance.insert(
+            &self.make_key(krate),
+            source,
+            &Utc::now().format("%a, %d %b %Y %T GMT").to_string(),
+        )?;
+        let (parts, body) = insert_req.into_parts();
+
+        let uri = parts.uri.to_string();
+        let builder = self.client.put(&uri);
+
+        let request = builder.headers(parts.headers).body(body).build()?;
+
+        self.client.execute(request).await?.error_for_status()?;
+
+        Ok(content_len as usize)
     }
 
     async fn list(&self) -> Result<Vec<String>, Error> {
-        let list = self
-            .client
-            .list_blobs()
-            .with_container_name(&self.container)
-            .with_include_copy()
-            .with_include_deleted()
-            .with_include_metadata()
-            .with_include_snapshots()
-            .with_include_uncommitted_blobs()
-            .finalize()
+        let list_req = self
+            .instance
+            .list(&Utc::now().format("%a, %d %b %Y %T GMT").to_string())?;
+
+        let (parts, _) = list_req.into_parts();
+
+        let uri = parts.uri.to_string();
+        let builder = self.client.get(&uri);
+
+        let request = builder.headers(parts.headers).build()?;
+        let response = self.client.execute(request).await?.error_for_status()?;
+        let resp_body = response
+            .text()
             .await
-            .context("failed to list objects")?;
-        Ok(list
-            .incomplete_vector
-            .vector
+            .context("failed to get list response")?;
+        let resp_body = resp_body.trim_start_matches('\u{feff}');
+        let resp = blob::parse_list_body(resp_body)?;
+        let a = resp
+            .blobs
+            .blob
             .into_iter()
-            .filter_map(|o| {
-                if o.name.starts_with(&self.prefix) {
-                    Some(o.name)
-                } else {
-                    None
-                }
-            })
-            .collect())
+            .map(|b| b.name)
+            .collect::<Vec<String>>();
+        Ok(a)
     }
 
     async fn updated(&self, krate: &Krate) -> Result<Option<chrono::DateTime<chrono::Utc>>, Error> {
-        let b = self
-            .client
-            .get_blob()
-            .with_container_name(&self.container)
-            .with_blob_name(&self.make_key(krate))
-            .finalize()
-            .await
-            .context("failed to get index blob")?;
-        Ok(b.blob.last_modified)
+        let request = self.instance.properties(
+            &self.make_key(krate),
+            &Utc::now().format("%a, %d %b %Y %T GMT").to_string(),
+        )?;
+        let (parts, _) = request.into_parts();
+
+        let uri = parts.uri.to_string();
+        let builder = self.client.head(&uri);
+
+        let request = builder.headers(parts.headers).build()?;
+
+        let response = self.client.execute(request).await?.error_for_status()?;
+        let properties = blob::PropertiesResponse::try_from(convert_response(response).await?)?;
+        let a = properties.last_modified;
+        let a = chrono::DateTime::parse_from_str(&a, "%a, %d %b %Y %T GMT")?;
+        Ok(Some(a.into()))
     }
 
     fn set_prefix(&mut self, prefix: &str) {
